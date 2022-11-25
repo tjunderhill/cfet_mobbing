4991a1da90fa9de357a111199e886c4938c93dc7
tjunderhill <tom.underhill7000@gmail.com>
2022-11-25 14:30:16 +0000

    more stuff

--- app.py
+++ app.py
@@ -10,13 +10,20 @@ def fooBarQix(number):
 
     strNumber = str(number)
     for c in range(0, len(strNumber)):
-        print(strNumber[c])
+        #print(strNumber[c])
         if strNumber[c] == '3':
             result += "Foo"
         if strNumber[c] == '5':
             result += "Bar"
         if strNumber[c] == '7':
             result += "Qix"
+
+    if len(result) == 0:
+        result+=str(number)
     return result
 
-print(fooBarQix(5))
+print(fooBarQix(101))
+
+assert fooBarQix(15) == "FooBarBar"
+assert fooBarQix(51) == "FooBar"
+assert fooBarQix(52) == "Bar"
